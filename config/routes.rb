d15358774fb45067e6b9721c9bcbb3a241ae8402
Lance Johnson <lancejjohnson@gmail.com>
2014-11-25 08:12:38 -0500

    Adds basic store listing of products

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Depot::Application.routes.draw do
+  # You can have the root of your site routed with "root"
+  # as: option generates the helper method `store_path`
+  root 'store#index', as: 'store'
+
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
