d51f937b0cb059fe2e8ff981c8f084edd8d3f020
Lance Johnson <lancejjohnson@gmail.com>
2014-11-28 14:27:20 -0500

    Adds line_items and the cart relationships

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
   def index
+    increment_counter
     @products = Product.order(:title)
+  end
+
+  private
+
+  def increment_counter
+    if session[:counter]
+      session[:counter] += 1
+    else
+      session[:counter] = 1
+    end
   end
 end
