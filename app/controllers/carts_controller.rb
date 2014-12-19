64f2c23ba26b38e5a27f3ae6dbe5b2d843c41dc0
Lance Johnson <lancejjohnson@gmail.com>
2014-12-19 07:12:29 -0500

    G1: Updates Cart checkout

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,28 +2,20 @@ class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
-  # GET /carts
-  # GET /carts.json
   def index
     @carts = Cart.all
   end
 
-  # GET /carts/1
-  # GET /carts/1.json
   def show
   end
 
-  # GET /carts/new
   def new
     @cart = Cart.new
   end
 
-  # GET /carts/1/edit
   def edit
   end
 
-  # POST /carts
-  # POST /carts.json
   def create
     @cart = Cart.new(cart_params)
 
@@ -38,8 +30,6 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1
-  # PATCH/PUT /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
@@ -52,8 +42,6 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1
-  # DELETE /carts/1.json
   def destroy
     @cart.destroy if @cart.id == session[:cart_id]
     session[:cart_id] = nil
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -12,8 +12,19 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
     get :new
     assert_response :success
+  end
+
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty.'
   end
 
   test "should create order" do
@@ -21,7 +32,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
